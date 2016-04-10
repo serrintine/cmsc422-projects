5811af5c665fbe233889bd3409a2c5314276570a
serrintine <serrintine@users.noreply.github.com>
2016-04-10 03:20:01 -0400

    started gradient descent

--- p2/gd.py
+++ p2/gd.py
@@ -26,20 +26,17 @@ def gd(func, grad, x0, numIter, stepSize):
     # begin iterations
     for iter in range(numIter):
         # compute the gradient at the current location
-        g = util.raiseNotDefined()    ### TODO: YOUR CODE HERE
-
+        g = grad(x)
 
         # compute the step size
-        eta = util.raiseNotDefined()    ### TODO: YOUR CODE HERE
-
+        eta = stepSize/sqrt(iter+1)
 
         # step in the direction of the gradient
-        x = util.raiseNotDefined()    ### TODO: YOUR CODE HERE
-
+        x = eta*g
 
         # record the trajectory
         trajectory[iter+1] = func(x)
-
+        
     # return the solution
     return (x, trajectory)
 
